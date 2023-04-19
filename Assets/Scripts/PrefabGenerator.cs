4714118721164c4ea719af704412ff6cfe3aedbc
iilocyi1 <124301687+iilocyi1@users.noreply.github.com>
2023-04-18 21:46:52 -0700

    Along the x Axis

--- Assets/Scripts/PrefabGenerator.cs
+++ Assets/Scripts/PrefabGenerator.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public Vector3 spawnValues;
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 1; i <= 10; i++)
+        {
+            Vector3 spawnposition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0f, 0f);
+           
+            GameObject gameobject = Instantiate<GameObject>(prefab, spawnposition, transform.rotation);
+
+        }
+        
         
     }
 
